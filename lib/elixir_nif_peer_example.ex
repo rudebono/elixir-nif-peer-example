306f52b6ca29e7f4647dd36c73eec87e175a2ecb
Hany Rock <rudebono@gmail.com>
2024-01-19 16:34:08 +0900

    remove default test.exs

--- lib/elixir_nif_peer_example.ex
+++ lib/elixir_nif_peer_example.ex
@@ -1,18 +1,2 @@
 defmodule ElixirNifPeerExample do
-  @moduledoc """
-  Documentation for `ElixirNifPeerExample`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirNifPeerExample.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/elixir_nif_peer_example_test.exs
+++ test/elixir_nif_peer_example_test.exs
@@ -1,8 +1,3 @@
 defmodule ElixirNifPeerExampleTest do
   use ExUnit.Case
-  doctest ElixirNifPeerExample
-
-  test "greets the world" do
-    assert ElixirNifPeerExample.hello() == :world
-  end
 end
